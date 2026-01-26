e8600173a7186c60a58e05213cb36a4a80d38fc4
TalaatMagdy <talaatmagdy75@gmail.com>
2026-01-26 04:23:32 +0200

    feat(slo): add with_slo_tracking decorators for sync/async SLO measurement v1.5.0

--- src/obskit/_version.py
+++ src/obskit/_version.py
@@ -24,7 +24,7 @@ Usage
 """
 
 # Version string
-__version__: str = "1.4.0"
+__version__: str = "1.5.0"
 
 # Version tuple for programmatic comparison
-__version_info__: tuple[int, int, int] = (1, 4, 0)
+__version_info__: tuple[int, int, int] = (1, 5, 0)
